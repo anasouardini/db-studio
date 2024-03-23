57a888079c8cbd455a89ae2ac2aa4f4b8e9c0986
anasouardini <ouardinianas@proton.me>
2024-03-23 17:57:20 +0000

    chore: add high-level fs

---
+++ packages/client/src/main.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('app entry point', () => {
+  it('fake test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/client/src/pages/profiles.tsx
@@ -0,0 +1,14 @@
+function ProfilesList() {
+  return <div>profiles</div>;
+}
+
+function Profiles() {
+  return (
+    <section>
+      <h1>Profiles</h1>
+      <ProfilesList />
+    </section>
+  );
+}
+
+export default Profiles;
